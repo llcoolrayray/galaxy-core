c25eafb8976e2a154548e7f2dc6dbcbd42e191b7
barney <wang.rui.barney@vertiv.com>
2024-01-22 10:23:11 +0800

    Initial commit

---
+++ src/main/java/com/micro/service/galaxycore/GalaxyCoreApplication.java
@@ -0,0 +1,13 @@
+package com.micro.service.galaxycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micro/service/galaxycore/GalaxyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.service.galaxycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxyCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
